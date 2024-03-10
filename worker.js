09e736a5078f57a2f058b95b992d2c2cf9a3541c
Lebogang <lebogangodeveloper@gmail.com>
2024-03-10 04:43:35 -0700

    Fix indentation error

--- worker.js
+++ worker.js
@@ -5,30 +5,30 @@ const dbClient = require('./utils/db');
 const fileQueue = new Queue('fileQueue');
 
 fileQueue.process(async (job) => {
-    const { userId, fileId } = job.data;
+  const { userId, fileId } = job.data;
 
-    // Check if fileId and userId are present
-    if (!fileId) {
-        throw new Error('Missing fileId');
-    }
-    if (!userId) {
-        throw new Error('Missing userId');
-    }
+  // Check if fileId and userId are present
+  if (!fileId) {
+    throw new Error('Missing fileId');
+  }
+  if (!userId) {
+    throw new Error('Missing userId');
+  }
 
-    // Find the document in the DB based on fileId and userId
-    const fileDocument = await dbClient.db.collection('files').findOne({ _id: fileId, userId: userId });
-    if (!fileDocument) {
-        throw new Error('File not found');
-    }
+  // Find the document in the DB based on fileId and userId
+  const fileDocument = await dbClient.db.collection('files').findOne({ _id: fileId, userId });
+  if (!fileDocument) {
+    throw new Error('File not found');
+  }
 
-    // Generate thumbnails
-    const options = { width: 500 };
-    const thumbnail500 = await imageThumbnail(fileDocument.localPath, options);
-    // Save or do something with the thumbnail500
+  // Generate thumbnails
+  const options = { width: 500 };
+  const thumbnail500 = await imageThumbnail(fileDocument.localPath, options);
+  // Save or do something with the thumbnail500
 
-    // Repeat for other thumbnail sizes (250, 100)
+  // Repeat for other thumbnail sizes (250, 100)
 
-    return { thumbnail500, /* other thumbnails */ };
+  return { thumbnail500 /* other thumbnails */ };
 });
 
 module.exports = fileQueue;
